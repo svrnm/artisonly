df135a9a63b99ef37891920ea4e5c19c1b6df113
Severin Neumann <severin.neumann@altmuehlnet.de>
2015-08-25 15:36:47 +0200

    Added Inspire Command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\Inspire;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Inspire::class
     ];
 
 	protected $includeDefaultCommands = false;
